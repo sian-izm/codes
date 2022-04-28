5bb27118759301d43cc945027a6781c775740198
sian.izm <sian.izm@gmail.com>
2022-04-28 23:20:13 +0700

    install moka

--- typescript/test/hello.test.ts
+++
@@ -1,9 +0,0 @@
-import TestTarget from '../src/hello';
-import { expect } from 'chai';
-
-describe("TestTarget", () => {
-    it("should have a name", () => {
-        let testTarget = new TestTarget("test");
-        assert.equal(testTarget.name, "test");
-    });
-});
