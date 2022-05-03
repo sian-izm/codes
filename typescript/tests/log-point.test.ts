0d282660c17bcf5a60f4e58a860859bbab753f6b
sian.izm <sian.izm@gmail.com>
2022-05-03 22:59:52 +0700

    added virtual point class test

--- typescript/tests/log-point.test.ts
+++ typescript/tests/log-point.test.ts
@@ -1,9 +1,14 @@
 import { expect } from "chai";
 import { logPoint } from "../src/log-point";
+import { VirtualPoint } from "../src/virtual-point";
 
 describe("logPoint", () => {
   it("should output log point", () => {
     const point = { x: 12, y: 10 };
     expect(logPoint(point)).to.equal("12, 10");
-  })
+  });
+  it("should output from virtual point class", () => {
+    const point = new VirtualPoint(10, 12);
+    expect(logPoint(point)).to.equal("10, 12");
+  });
 })
