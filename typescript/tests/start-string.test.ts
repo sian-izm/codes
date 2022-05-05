f37f7e87f63a553991eef8cad67b6d17bbf9d635
sian.izm <sian.izm@gmail.com>
2022-05-05 23:32:57 +0700

    define test return string by any arg

--- typescript/tests/start-string.test.ts
+++ typescript/tests/start-string.test.ts
@@ -4,5 +4,17 @@ import { expect } from "chai";
 describe("start-string", () => {
   it("should return string by string arg", () =>{
     expect(start("HOGE")).to.equal("HOGE");
-  })
+  });
+  it("should return string by string array arg", () =>{
+    expect(start(["HOGE", "HUGA"])).to.equal("HOGE,HUGA");
+  });
+  it("should return string by function returning string", () =>{
+    const func = function() {
+      return "HOGEHOGE";
+    }
+    expect(start(func())).to.equal("HOGEHOGE");
+  });
+  it("should return string by string dictionary", () =>{
+    expect(start({s: "Mogo"})).to.equal("Mogo");
+  });  
 })
