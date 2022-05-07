c0052e9aecde34ab4aa952f3c1802e5a468b171a
sian.izm <sian.izm@gmail.com>
2022-05-07 22:59:59 +0700

    test uppercase

---
+++ typescript/tests/uppercase.test.ts
@@ -0,0 +1,9 @@
+import { expect } from "chai";
+import { uppercase } from "../src/uppercase";
+
+describe("uppercase", () => {
+  it("should return uppercase", () => {
+    const str = "hogehoge";
+    expect(uppercase(str)).to.equal("Hogehoge");
+  })
+})
