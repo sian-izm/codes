04524d5eeda59fffe92969785c68d41331e778e2
sian.izm <sian.izm@gmail.com>
2022-05-30 21:48:53 +0700

    test parse into string

--- typescript/tests/map.test.ts
+++ typescript/tests/map.test.ts
@@ -2,8 +2,12 @@ import { expect } from "chai";
 import { mapAlias } from "../src/map";
 
 describe("mapAlias", () => {
-  it('should behave map function', () => {
+  it('should return int', () => {
     const parsed = mapAlias(["1", "2", "3"], (n) => parseInt(n));
     expect(parsed).to.deep.equal([1,2,3]);
   });
+  it('should return string', () => {
+    const parsed = mapAlias([1, 2, 3], (n) => String(n));
+    expect(parsed).to.deep.equal(['1','2','3']);
+  })
 })
