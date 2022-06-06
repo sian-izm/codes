2a42137142e76360818023e04b7210f02baec684
sian.izm <sian.izm@gmail.com>
2022-06-06 23:17:35 +0700

    define insertInBegin

--- typescript/src/linked-list.ts
+++ typescript/src/linked-list.ts
@@ -1,3 +1,18 @@
 import { LinkedListInterface } from "./linked-list.interface";
+import { Node } from "./node";
 
-export class LinkedList<T> implements LinkedListInterface<T> {}
+export class LinkedList<T> implements LinkedListInterface<T> {
+  private head: Node<T> | null = null;
+
+  public insertInBegin(data: T): Node<T> {
+    const node = new Node(data);
+    if (!this.head) {
+      this.head = node;
+    } else {
+      this.head.prev = node;
+      node.next = this.head;
+      this.head = node;
+    }
+    return node;
+  }
+}
