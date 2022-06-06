95a28b6e0a0048133a8948180d930d6773dc8d88
sian.izm <sian.izm@gmail.com>
2022-06-06 23:24:51 +0700

    define test linked list

---
+++ typescript/tests/linked-list.test.ts
@@ -0,0 +1,11 @@
+import { LinkedList } from "../src/linked-list";
+import { Node } from "../src/node";
+import { Post } from "../src/post";
+import { expect } from "chai";
+
+describe("linked list", () => {
+  it('should insert in begin', () => {
+    const node = new Node<Post>();
+    const linkedList = LinkedList.new()
+  })
+})
