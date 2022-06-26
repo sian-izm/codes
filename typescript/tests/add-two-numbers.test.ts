ff52023b0575754b2cc43fc0eb67dce4ec7676a3
sian.izm <sian.izm@gmail.com>
2022-06-26 22:41:06 +0700

    test add-two-numbers

---
+++ typescript/tests/add-two-numbers.test.ts
@@ -0,0 +1,13 @@
+import { expect } from 'chai';
+import { addTwoNumbers } from '../src/add-two-numbers';
+import { LinkedList } from '../src/linked-list'
+
+describe('addTwoNumbers linked list', () => {
+  it('should return added two nodes', () => {
+    const l1 = new LinkedList<number>()
+    l1.insertAtEnd(1)
+    const l2 = new LinkedList<number>()
+    l2.insertAtEnd(2)
+    expect(addTwoNumbers(l1,l2)).to.equal([3]);
+  })
+})
