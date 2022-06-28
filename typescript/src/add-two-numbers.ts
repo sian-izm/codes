6c6997434496d71734b979d25486af76b9b05934
sian.izm <sian.izm@gmail.com>
2022-06-29 00:25:37 +0700

    define addTwoNumbers

--- typescript/src/add-two-numbers.ts
+++ typescript/src/add-two-numbers.ts
@@ -2,5 +2,12 @@ import { Node } from "./node";
 import { LinkedList } from "../src/linked-list";
 
 export function addTwoNumbers(l1: LinkedList<number> | null, l2: LinkedList<number> | null ): LinkedList<number> | null {
-  return null;
+  const l1Number = Number(l1.traverse().join(''))
+  const l2Number = Number(l2.traverse().join(''))
+  const sumArray = String(l1Number + l2Number).split('').map(Number)
+  const linkedList = new LinkedList<number>()
+  sumArray.forEach(function (value) {
+    linkedList.insertAtEnd(value)
+  })
+  return linkedList
 }
--- typescript/tests/add-two-numbers.test.ts
+++ typescript/tests/add-two-numbers.test.ts
@@ -6,8 +6,9 @@ describe('addTwoNumbers linked list', () => {
   it('should return added two nodes', () => {
     const l1 = new LinkedList<number>()
     l1.insertAtEnd(1)
+    l1.insertAtEnd(1)
     const l2 = new LinkedList<number>()
     l2.insertAtEnd(2)
-    expect(addTwoNumbers(l1,l2)).to.equal([3]);
+    expect(addTwoNumbers(l1,l2).traverse()).to.deep.equal([1,3]);
   })
 })
