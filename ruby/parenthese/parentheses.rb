a42e52454a5094db41bf3d84cb29691e4d341bfd
sian.izm <sian.izm@gmail.com>
2022-07-06 20:00:43 +0700

    implement ({})

--- ruby/parenthese/parentheses.rb
+++ ruby/parenthese/parentheses.rb
@@ -7,31 +7,49 @@ class Parenthes
   CLOSE_BRACKET = ']'
  
   def self.valid?(s)
-    corresponding_close = ''
+    corresponding_close = []
     s.each_char do |str|
-      if corresponding_close == ''
-        corresponding_close = case str
-                when OPEN_BRACE
-                  CLOSE_BRACE
-                when OPEN_BRACKET
-                  CLOSE_BRACE
-                when OPEN_PARENTH
-                  CLOSE_PARENTH
-                end
-      elsif [CLOSE_BRACE, CLOSE_BRACKET, CLOSE_PARENTH].include?(corresponding_close)
+      if corresponding_close == [] && find_corresponding_close(str)
+        corresponding_close.push(find_corresponding_close(str))
+      elsif (closes & corresponding_close) != []
         case str
-        when OPEN_BRACE, OPEN_BRACKET, OPEN_PARENTH, *not_corresponding_close(corresponding_close)
+        when *not_corresponding_close(corresponding_close)
+          puts str
+          puts corresponding_close
           return false
-        when corresponding_close
-          corresponding_close = ''
-        end        
+        when corresponding_close.last
+          corresponding_close.pop
+        end
       end
     end
-    true
+    if corresponding_close == []
+      true
+    else
+      false
+    end
+  end
+
+  def self.find_corresponding_close(open_str)
+    case open_str
+    when OPEN_BRACE
+      CLOSE_BRACE
+    when OPEN_BRACKET
+      CLOSE_BRACKET
+    when OPEN_PARENTH
+      CLOSE_PARENTH
+    end
+  end
+
+  def self.opens
+    [:BRACE, :BRACKET, :PARENTH].map {|str| eval("OPEN_#{str}")}
+  end
+
+  def self.closes
+    [:BRACE, :BRACKET, :PARENTH].map {|str| eval("CLOSE_#{str}")}
   end
 
   def self.not_corresponding_close(close)
-    closes = [CLOSE_BRACE, CLOSE_BRACKET, CLOSE_PARENTH]
+    closes = [:BRACE, :BRACKET, :PARENTH].map {|str| eval("CLOSE_#{str}")}
     closes.delete(close)
     closes
   end
--- ruby/parenthese/test_parentheses.rb
+++ ruby/parenthese/test_parentheses.rb
@@ -3,7 +3,7 @@ require 'minitest/autorun'
 
 class TestValidParentheses < Minitest::Test
   def test_valid_single_parenthes
-    str = 'abc()'
+    str = '(abc)'
     assert_equal true, Parenthes.valid?(str)
   end
 
@@ -18,10 +18,32 @@ class TestValidParentheses < Minitest::Test
   end
 
   def test_valid_mutiple_type_parenthes
+    str = '(ab)cd{ef}'
+    assert_equal true, Parenthes.valid?(str)
   end
 
   def test_invalid_parenthes
     str = '(a(bc'
     assert_equal false, Parenthes.valid?(str)
+  end
+
+  def test_invalid_opens
+    str = '(a{b]c}'
+    assert_equal false, Parenthes.valid?(str)
+  end
+
+  def test_no_parenthes
+    str = ''
+    assert_equal true, Parenthes.valid?(str)
+  end
+
+  def test_only_multiple_parentes
+    str = '(){}[]'
+    assert_equal true, Parenthes.valid?(str)
+  end
+
+  def test_nested_parenthes
+    str = '{[]}'
+    assert_equal true, Parenthes.valid?(str)
   end
 end
