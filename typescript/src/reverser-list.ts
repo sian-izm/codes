7c3c98745d5568850765e101c7b24e7eafceeb01
sian.izm <sian.izm@gmail.com>
2022-07-09 15:24:36 +0700

    move the Node class

--- typescript/src/reverser-list.ts
+++ typescript/src/reverser-list.ts
@@ -1,3 +1,12 @@
+class Node {
+  val: number
+  next: Node | null
+  constructor(val?: number, next?: Node | null) {
+    this.val = (val===undefined ? 0 : val)
+    this.next = (next===undefined ? null : next)
+  }
+}
+
 export function reverseList(head: Node | null): Node | null {
   if (!head.next) return head
   const array: number[] = traverse(head)
@@ -9,20 +18,11 @@ export function reverseList(head: Node | null): Node | null {
 function traverse(head: Node): number[] {
   const result = new Array<number>()
   const pushData = (node: Node): void => {
-    result.push(node.data)
+    result.push(node.val)
     if (node.next) {
       pushData(node.next)
     }
   }
   pushData(head)
   return result
-}
-
-class Node {
-  val: number
-  next: Node | null
-  constructor(val?: number, next?: Node | null) {
-    this.val = (val===undefined ? 0 : val)
-    this.next = (next===undefined ? null : next)
-  }
 }
