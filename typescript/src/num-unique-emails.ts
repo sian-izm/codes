0021d142bf6a30f19f356d75d114aadb3a266b7f
sian.izm <sian.izm@gmail.com>
2022-07-27 22:35:56 +0700

    fix the state of domain

--- typescript/src/num-unique-emails.ts
+++ typescript/src/num-unique-emails.ts
@@ -2,11 +2,10 @@ export function numUniqueEmails(emails: string[]): number {
   const uniqEmails: {[key: string]: number} = {}
   emails.forEach(email => {
     const splitedEmail = email.split('@')
-    console.log(splitedEmail)
     const n = splitedEmail[0].indexOf('+')
-    const name = splitedEmail[0].substring(0, n !== -1 ? n : splitedEmail[0].length)
-    uniqEmails[name] = 1
+    const name = splitedEmail[0].substring(0, n !== -1 ? n : splitedEmail[0].length).split('.').join('')
+    const uniqEmail = name + '@' + splitedEmail[1]
+    uniqEmails[uniqEmail] = 1
   })
-  console.log(uniqEmails)
   return Object.keys(uniqEmails).length
 }
--- typescript/tests/num-unique-emails.test.ts
+++ typescript/tests/num-unique-emails.test.ts
@@ -5,4 +5,17 @@ describe('numUniqueEmails', () => {
   it('should return 2 for all unque emails', () => {
     expect(numUniqueEmails(['sian.izm@gmail.com', 'hoge@huga.com'])).to.equal(2)
   })
+
+  it('remove + after text, remove .', () => {
+    expect(numUniqueEmails(
+      ["test.email+alex@leetcode.com",
+      "test.e.mail+bob.cathy@leetcode.com",
+      "testemail+david@lee.tcode.com"])).to.equal(2)
+  })
+
+  it('differentiate . for domain', () => {
+    expect(numUniqueEmails(
+      ["testemail@leetcode.com", "testemail@leet.code.com"]
+    )).to.equal(2)
+  })
 })
