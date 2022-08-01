7cc84151fe72ca62b61a7bbd2c17422a61122fd7
sian.izm <sian.izm@gmail.com>
2022-08-01 23:34:22 +0700

    define subarraySum

--- typescript/src/subarray-sum.ts
+++ typescript/src/subarray-sum.ts
@@ -1,3 +1,35 @@
 export function subarraySum(nums: number[], k: number): number {
-  return 2
+  let result = 0
+  let valueSum = 0
+  const recursive = (num: number, i) => {
+    result = +1 
+    if (nums[i+1] === 0) {
+      recursive
+    }
+  }
+  nums.forEach((value,index)=>{
+    valueSum += value
+    if (valueSum === k) {
+      result += 1
+      valueSum = 0
+    } else if (valueSum <k) {
+      for(let i=index+1;i<nums.length;i++) {
+        valueSum += nums[i]
+        if (valueSum === k ) {
+          result += 1
+          valueSum = 0
+          break
+        } else if (valueSum>k) {
+          valueSum = 0
+          break
+        }
+        if (i===(nums.length-1)) {
+          valueSum = 0
+        }
+      }
+    } else {
+      valueSum = 0
+    }
+  })
+  return result
 }
--- typescript/tests/subarray-sum.test.ts
+++ typescript/tests/subarray-sum.test.ts
@@ -5,4 +5,10 @@ describe('subarraySum', () => {
   it('should return 2', () => {
     expect(subarraySum([1,1,1],2)).to.equal(2)
   })
+  it('should return 4', () => {
+    expect(subarraySum([1,2,1,2,1],3)).to.equal(4)
+  })
+  it('should return 4', () => {
+    expect(subarraySum([-1,-1,1],1)).to.equal(1)
+  })  
 })
