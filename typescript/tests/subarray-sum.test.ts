aea99e2b00c9c04015b4a0a1a1e223f119e3a24d
sian.izm <sian.izm@gmail.com>
2022-08-02 20:48:35 +0700

    fixed test cases

--- typescript/tests/subarray-sum.test.ts
+++ typescript/tests/subarray-sum.test.ts
@@ -8,7 +8,22 @@ describe('subarraySum', () => {
   it('should return 4', () => {
     expect(subarraySum([1,2,1,2,1],3)).to.equal(4)
   })
-  it('should return 4', () => {
+  it('should return 1', () => {
     expect(subarraySum([-1,-1,1],1)).to.equal(1)
-  })  
+  })
+  it('should return 3', () => {
+    expect(subarraySum([-1,1,0],0)).to.equal(3)
+  })
+  it('should return 3', () => {
+    expect(subarraySum([0,0],0)).to.equal(3)
+  })
+  it('should return 3', () => {
+    expect(subarraySum([1,0,0],1)).to.equal(3)
+  })
+  it('should return 3', () => {
+    expect(subarraySum([0,1,0],0)).to.equal(2)
+  })
+  it('should return 3', () => {
+    expect(subarraySum([1,0,0,0,0],0)).to.equal(10)
+  })
 })
