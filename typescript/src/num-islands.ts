03102cbf8edab82523bebcc6d8309319f37ece6b
sian.izm <sian.izm@gmail.com>
2022-08-05 23:44:36 +0700

    define group

--- typescript/src/num-islands.ts
+++ typescript/src/num-islands.ts
@@ -1,3 +1,22 @@
-export function numIslans(grip: string[][]): number {
+export function numIslands(grid: string[][]): number {
+  const column = grid.length 
+  const row = grid[0].length
+
+  const indices = {}
+  for (let i=0;i<column;i++) {
+    for(let j=0;j<row;j++) {
+      indices["${i},${j}"] = 1
+    }
+  }
+  const group = {}
+  const groupKey = 1
+  Object.keys(indices).forEach(key => {
+    const index = key.split(',')
+    if (grid[index[0]][index[1]] === '1') {
+      group[groupKey].push()
+    } else {
+
+    }
+  }) 
   return 1
 }
--- typescript/tests/num-islands.test.ts
+++ typescript/tests/num-islands.test.ts
@@ -1,8 +1,8 @@
 import { expect } from 'chai'
-import { numIslans } from '../src/num-islands'
+import { numIslands } from '../src/num-islands'
 
 describe('numIsland', () => {
   it('should return 1', () => {
-    expect(numIslans([['1']])).to.equal(1)
+    expect(numIslands([['1']])).to.equal(1)
   })
 })
