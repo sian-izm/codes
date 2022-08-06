7597e1f80a3cfa69d671817f3167b54b66c9341e
sian.izm <sian.izm@gmail.com>
2022-08-06 23:14:10 +0700

    define ladderLendght

---
+++ typescript/tests/ladder-length.test.ts
@@ -0,0 +1,8 @@
+import { expect } from 'chai'
+import { ladderLength } from '../src/ladder-length'
+
+describe('ladderLength', () => {
+  it('should return 5', () => {
+    expect(ladderLength('hit', 'cog', ["hot","dot","dog","lot","log","cog"])).to.equal(5)
+  })
+})
