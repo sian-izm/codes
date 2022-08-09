a96cdacc55858b78338929b77c161c1f24c166ed
sian.izm <sian.izm@gmail.com>
2022-08-09 22:45:28 +0700

    defined minDepth"

--- typescript/src/min-depth.ts
+++ typescript/src/min-depth.ts
@@ -1,5 +1,21 @@
 import { TreeNode } from './tree-node'
 
 export function minDepth(root: TreeNode | null): number {
-  return 1
-};
+  return root ? findNestedDepth(root, 1) : 0
+}
+
+function findNestedDepth(node: TreeNode, tmpDepth: number): number {
+  let result = tmpDepth
+  if (node.right) {
+    const nestedDepth = findNestedDepth(node.right, tmpDepth + 1)
+    result = nestedDepth
+  }
+
+  if (node.left) {
+    const nestedDepth = findNestedDepth(node.left, tmpDepth + 1)
+    if (result === tmpDepth || nestedDepth < result) {
+      result = nestedDepth
+    }
+  }
+  return result
+}
