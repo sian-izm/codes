78086a0c13d70e67b0625274cc915f5e9f4949e5
sian.izm <sian.izm@gmail.com>
2022-08-10 22:00:49 +0700

    remove redundant null

--- typescript/tests/min-depth.test.ts
+++ typescript/tests/min-depth.test.ts
@@ -4,30 +4,30 @@ import { TreeNode } from '../src/tree-node'
 
 describe('minDepth', () => {
   it('should return 2-1', () => {
-    const node5 = new TreeNode(7,null,null)
-    const node4 = new TreeNode(15,null,null)
+    const node5 = new TreeNode(7)
+    const node4 = new TreeNode(15)
     const node3 = new TreeNode(20,node4,node5)
-    const node2 = new TreeNode(9,null,null)
+    const node2 = new TreeNode(9)
     const node1 = new TreeNode(3,node3,node2)
     expect(minDepth(node1)).to.equal(2)
   })
 
   it('should return 5', () => {
-    const node5 = new TreeNode(6,null,null)
-    const node4 = new TreeNode(5,node5,null)
-    const node3 = new TreeNode(4,node4,null)
-    const node2 = new TreeNode(3,node3,null)
-    const node1 = new TreeNode(2,node2,null)
+    const node5 = new TreeNode(6)
+    const node4 = new TreeNode(5,node5)
+    const node3 = new TreeNode(4,node4)
+    const node2 = new TreeNode(3,node3)
+    const node1 = new TreeNode(2,node2)
     expect(minDepth(node1)).to.equal(5)
   })
 
   it('should return 2-2', () => {
-    const node2 = new TreeNode(2,null,null)
-    const node1 = new TreeNode(1,node2,null)
+    const node2 = new TreeNode(2)
+    const node1 = new TreeNode(1,node2)
     expect(minDepth(node1)).to.equal(2)
   })
   it('should return 2-3', () => {
-    const node2 = new TreeNode(2,null,null)
+    const node2 = new TreeNode(2)
     const node1 = new TreeNode(1,null,node2)
     expect(minDepth(node1)).to.equal(2)
   })
