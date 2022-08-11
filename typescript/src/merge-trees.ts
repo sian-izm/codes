8ca58ab8cfb25dd9753174378ced640151d792c7
sian.izm <sian.izm@gmail.com>
2022-08-11 22:51:16 +0700

    defining init num

--- typescript/src/merge-trees.ts
+++ typescript/src/merge-trees.ts
@@ -1,13 +1,23 @@
 import { TreeNode } from './tree-node'
 export function mergeTrees(root1: TreeNode | null, root2: TreeNode | null): TreeNode {
   let result = new TreeNode()
-  const mergeNodes = (node1: TreeNode, node2: TreeNode): void => {
-    const right: number = node1.right.val + node2.right.val
-    const left: number = node1.right.val + node2.right.val
-    result.right = new TreeNode(right)
-    result.left = new TreeNode(left)
-    mergeNodes(node1.right, node2.right)
-    mergeNodes(node1.left, node2.left)
+  const mergeNodes = (node1: TreeNode, node2: TreeNode, appendedNode: TreeNode): void => {
+    const right: number = rightNum(node1) + rightNum(node2)
+    const left: number = leftNum(node1) + leftNum(node2)
+    appendedNode.right = new TreeNode(right)
+    appendedNode.left = new TreeNode(left)
+    mergeNodes(node1 ? node1.right : null, node2 ? node2.right : null, appendedNode)
+    mergeNodes(node1 ? node1.left : null, node2 ? node2.left: null, appendedNode)
   }
+
+  mergeNodes(root1, root2, result)
   return result
+}
+
+function rightNum(node: TreeNode | null): number {
+  return node ? (node.right ? node.right.val : 0 ) : 0
+}
+
+function leftNum(node: TreeNode | null): number {
+  return node ? (node.right ? node.right.val : 0 ) : 0
 }
