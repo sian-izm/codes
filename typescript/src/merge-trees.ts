08c2207d2c00d60d22ef38622da1e379722adc6a
sian.izm <sian.izm@gmail.com>
2022-08-13 23:51:16 +0700

    seperate into function

--- typescript/src/merge-trees.ts
+++ typescript/src/merge-trees.ts
@@ -1,21 +1,21 @@
 import { TreeNode } from './tree-node'
 export function mergeTrees(root1: TreeNode | null, root2: TreeNode | null): TreeNode {
-  const mergeNodes = (node1: TreeNode, node2: TreeNode): TreeNode | null => {
-    const val = initNum(node1) + initNum(node2)
-    let tmpNode = new TreeNode(val)
-    if (node1 || node2) {
-      tmpNode.right = mergeNodes(node1 ? node1.right : null, node2 ? node2.right : null)
-      tmpNode.left = mergeNodes(node1 ? node1.left : null, node2 ? node2.left : null)
-    }
-    return tmpNode
-  }
   const val = root1.val + root2.val
   const result = new TreeNode(val)
-  result.right = mergeNodes(root1.right, root2.right)
-  result.left = mergeNodes(root1.left, root2.left)
+  result.right = mergedTree(root1.right, root2.right)
+  result.left = mergedTree(root1.left, root2.left)
   return result
 }
 
+function mergedTree(node1?: TreeNode, node2?: TreeNode): TreeNode | null {
+  const val = initNum(node1) + initNum(node2)
+  let tmpNode = new TreeNode(val)
+  if (node1 || node2) {
+    tmpNode.right = mergedTree(node1 ? node1.right : null, node2 ? node2.right : null)
+    tmpNode.left = mergedTree(node1 ? node1.left : null, node2 ? node2.left : null)
+  }
+  return tmpNode
+}
 function initNum(node: TreeNode | null): number {
   return node ? node.val : 0
 }
