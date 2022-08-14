c8efea428001fd4585114d1a46f28a5192f56c62
sian.izm <sian.izm@gmail.com>
2022-08-14 23:20:30 +0700

    fixed mergeTrees

--- typescript/src/merge-trees.ts
+++ typescript/src/merge-trees.ts
@@ -1,21 +1,29 @@
 import { TreeNode } from './tree-node'
 export function mergeTrees(root1: TreeNode | null, root2: TreeNode | null): TreeNode {
-  const val = root1.val + root2.val
-  const result = new TreeNode(val)
-  result.right = mergedTree(root1.right, root2.right)
-  result.left = mergedTree(root1.left, root2.left)
-  return result
+  if (!root1 && !root2) { return null }
+  return mergedTree(root1, root2)
 }
 
 function mergedTree(node1?: TreeNode, node2?: TreeNode): TreeNode | null {
   const val = initNum(node1) + initNum(node2)
   let tmpNode = new TreeNode(val)
-  if (node1 || node2) {
-    tmpNode.right = mergedTree(node1 ? node1.right : null, node2 ? node2.right : null)
-    tmpNode.left = mergedTree(node1 ? node1.left : null, node2 ? node2.left : null)
+  if (isExistNum(node1?.right) || isExistNum(node2?.right)) {
+    tmpNode.right = mergedTree(node1?.right, node2?.right)
+  }
+  if (isExistNum(node1?.left) || isExistNum(node2?.left)) {
+    tmpNode.left = mergedTree(node1?.left, node2?.left)
   }
   return tmpNode
 }
+
 function initNum(node: TreeNode | null): number {
-  return node ? node.val : 0
+  return node?.val || 0
+}
+
+function isExistNum(node: TreeNode | null): boolean {
+  if (node?.val || node?.val == 0) {
+    return true
+  } else {
+    return false
+  }
 }
--- typescript/tests/merge-trees.test.ts
+++ typescript/tests/merge-trees.test.ts
@@ -4,9 +4,9 @@ import { TreeNode } from '../src/tree-node'
 
 describe('mergeTrees', () => {
   it('should return 3 level nestedTree', () => {
-    const node4 = new TreeNode(5,null,null)
+    const node4 = new TreeNode(5)
     const node3 = new TreeNode(3,null,node4)
-    const node2 = new TreeNode(2,null,null)
+    const node2 = new TreeNode(2)
     const tree1 = new TreeNode(1,node2,node3)
 
     const node13 = new TreeNode(4)
@@ -15,12 +15,23 @@ describe('mergeTrees', () => {
     const node5 = new TreeNode(3,node7)
     const tree2 = new TreeNode(2,node5,node6)
 
-    const node12 = new TreeNode(5,null,null)
-    const node11 = new TreeNode(4,null,null)
+    const node12 = new TreeNode(5)
+    const node11 = new TreeNode(4)
     const node10 = new TreeNode(4,node11,node12)
-    const node9 = new TreeNode(7,null,null)
-    const node8 = new TreeNode(5,node9,null)
+    const node9 = new TreeNode(7)
+    const node8 = new TreeNode(5,node9)
     const result = new TreeNode(3,node8,node10)
-    expect(mergeTrees(tree1, tree2)).to.equal(result)
+    expect(mergeTrees(tree1, tree2)).to.deep.equal(result)
+  })
+
+  it('should return 3 level nestedTree', () => {
+    expect(mergeTrees(null, null)).to.deep.equal(null)
+  })
+
+  it('should return 3 level nestedTree', () => {
+    const tree1 = new TreeNode(0)
+    const tree2 = new TreeNode(0)
+    const result = new TreeNode(0)
+    expect(mergeTrees(tree1, tree2)).to.deep.equal(result)
   })
 })
