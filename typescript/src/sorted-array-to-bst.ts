3b5d8264e50971b6ddfc26fbf78e630f5597d626
sian.izm <sian.izm@gmail.com>
2022-08-15 22:56:56 +0700

    defining sortedArrayToBST

--- typescript/src/sorted-array-to-bst.ts
+++ typescript/src/sorted-array-to-bst.ts
@@ -1,5 +1,13 @@
 import { TreeNode } from "./tree-node";
 
 export function sortedArrayToBST(nums: number[]): TreeNode | null {
+  const length = nums.length
+  const result = new TreeNode(nums[Math.trunc(length/2)])
+  result.right = buildTreeNode(nums[0...Math.trunc(length/2)]))
+  result.left = buildTreeNode(nums[Math.trunc(length/2)...-1]))
+  return null
+}
+
+function buildTreeNode(nums: number[]): TreeNode | null {
   return null
 }
---
+++ typescript/tests/sorted-array-to-bst.test.ts
@@ -0,0 +1,15 @@
+import { expect } from 'chai'
+import { sortedArrayToBST } from '../src/sorted-array-to-bst'
+import { TreeNode } from '../src/tree-node'
+
+describe('sortedArrayToBST', () => {
+  it('should return 2 nested level tree', () => {
+    const node4 = new TreeNode(-10)
+    const node3 = new TreeNode(9,node4)
+    const node2 = new TreeNode(-10)
+    const node1 = new TreeNode(-3,node2)
+    const result = new TreeNode(0,node1,node3)
+
+    expect(sortedArrayToBST([-10,-3,0,5,9])).to.equal(result)
+  })
+})
