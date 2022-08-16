ee1edf23c5bbd5b77655e09fe45ade6e1071a81e
sian.izm <sian.izm@gmail.com>
2022-08-16 22:23:59 +0700

    define recursive method

--- typescript/src/sorted-array-to-bst.ts
+++ typescript/src/sorted-array-to-bst.ts
@@ -1,13 +1,19 @@
 import { TreeNode } from "./tree-node";
 
 export function sortedArrayToBST(nums: number[]): TreeNode | null {
-  const length = nums.length
-  const result = new TreeNode(nums[Math.trunc(length/2)])
-  result.right = buildTreeNode(nums[0...Math.trunc(length/2)]))
-  result.left = buildTreeNode(nums[Math.trunc(length/2)...-1]))
-  return null
+  return buildTreeNode(nums)
 }
 
 function buildTreeNode(nums: number[]): TreeNode | null {
-  return null
+  const length = nums.length
+  console.log('---------')
+  console.log(length)
+  console.log(nums.slice(0,Math.trunc(length/2)))
+  console.log(nums.slice(Math.round(length/2),length))
+  const tempResult = new TreeNode(nums[Math.trunc(length/2)])
+  if (length > 1) {
+    tempResult.right = buildTreeNode(nums.slice(0,Math.trunc(length/2)))
+    tempResult.left = buildTreeNode(nums.slice(Math.round(length/2),length))
+  }
+  return tempResult
 }
