58283b7d5bae3e1df046abe4ea4985897b872c7e
sian.izm <sian.izm@gmail.com>
2022-08-19 23:03:00 +0700

    define tree-node-right

--- typescript/src/tree-node-right.ts
+++ typescript/src/tree-node-right.ts
@@ -2,7 +2,7 @@ export class TreeNode {
   val: number
   right?: TreeNode
   left?: TreeNode
-  constructor(val?: number, right?: TreeNode, left?: TreeNode)  {
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null)  {
     this.val = (val===undefined ? 0 : val)
     this.right = (right===undefined ? null : right)
     this.left = (left===undefined ? null : left)
