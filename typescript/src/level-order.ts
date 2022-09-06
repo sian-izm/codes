6ec2279a6f5dd50e27d409648f9fff9eed8020b0
sian.izm <sian.izm@gmail.com>
2022-09-06 22:58:45 +0900

    fixed test case of level-order

--- typescript/src/level-order.ts
+++ typescript/src/level-order.ts
@@ -1,14 +1,21 @@
 import { TreeNode } from "./tree-node-right";
 
-export function levelOrder(root?: TreeNode): number[][] {
+export function levelOrder(root: TreeNode | null): number[][] {
   let currentLevel = 0
   const result: number[][] = []
 
-  const put = (node?: TreeNode, level: number): void => {
-    level
+  const put = (node: TreeNode | null, level: number): void => {
+    if (node) {
+      if (!result[level]) {
+        result[level] = []
+      }
+      result[level].push(node.val)
+      if (node.left) { put(node.left, level + 1) }
+      if (node.right) { put(node.right, level + 1) }
+    }
   }
 
   put(root, currentLevel)
 
-  return [[3],[9,20],[15,7]]
+  return result
 };
--- typescript/tests/level-order.test.ts
+++ typescript/tests/level-order.test.ts
@@ -4,7 +4,7 @@ import { TreeNode } from '../src/tree-node-right'
 
 describe('levelOrder', () => {
   it('should return 3 level tree node', () => {
-    const node4 = new TreeNode(17)
+    const node4 = new TreeNode(7)
     const node3 = new TreeNode(15)
     const node2 = new TreeNode(20,node3,node4)
     const node1 = new TreeNode(9)
