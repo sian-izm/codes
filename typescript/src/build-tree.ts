5e9d0619dc20a3fdaf01e86079bc950f065ce15d
sian.izm <sian.izm@gmail.com>
2022-10-17 21:15:32 +0700

    develop build-tree

---
+++ typescript/src/build-tree.ts
@@ -0,0 +1,5 @@
+import { TreeNode } from "./tree-node-right";
+
+export function buildTree(preorder: number[], inorder: number[]): TreeNode | null {
+  return new TreeNode(-1)
+}
---
+++ typescript/tests/build-tree.test.ts
@@ -0,0 +1,24 @@
+import { expect } from 'chai'
+import { TreeNode } from '../src/tree-node-right'
+import { buildTree } from '../src/build-tree'
+
+describe('buildTree', () => {
+  it('should return 1 level tree', () => {
+    const preorder = [-1]
+    const inorder = [-1]
+    const result = new TreeNode(-1)
+    expect(buildTree(preorder, inorder)).to.deep.equal(result)
+  })
+
+  it('should return 3 level tree', () => {
+    const preorder = [3,9,20,15,7]
+    const inorder = [9,3,15,20,7]
+
+    const node4 = new TreeNode(7)
+    const node3 = new TreeNode(15)
+    const node2 = new TreeNode(20,node3,node4)
+    const node1 = new TreeNode(9)
+    const result = new TreeNode(3,node1, node2)
+    expect(buildTree(preorder, inorder)).to.equal(result)
+  })
+})
