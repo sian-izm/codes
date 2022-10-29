9f724d40abca44867461449624d6b8ab2c2aa121
sian.izm <sian.izm@gmail.com>
2022-10-29 19:49:28 +0700

    save

--- typescript/src/build-tree.ts
+++ typescript/src/build-tree.ts
@@ -1,5 +1,10 @@
 import { TreeNode } from "./tree-node-right";
 
 export function buildTree(preorder: number[], inorder: number[]): TreeNode | null {
-  return new TreeNode(-1)
+  const root = preorder[0]
+  const rootIndex = inorder.findIndex(root)
+  const result = new TreeNode(root, leftNode(preorder[rootIndex+1], inorder[0...rootIndex]), rightNode(preorder[],inorder[rootIndex...-1]))
+  return result
 }
+
+function leftNode() {}
