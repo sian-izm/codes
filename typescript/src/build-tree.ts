64003ba7cbfd1b4e1a1501010cff78b420aad968
sian.izm <sian.izm@gmail.com>
2022-11-02 21:50:29 +0700

    debugging

--- typescript/src/build-tree.ts
+++ typescript/src/build-tree.ts
@@ -6,19 +6,24 @@ export function buildTree(preorder: number[], inorder: number[]): TreeNode | nul
     const inorderIndex: number = inorderVals.indexOf(val)
     remainingPreorder.shift()
     tree.val = val
-
     if (remainingPreorder.length) {
       if (inorder.slice(0,inorderIndex).length) {
         tree.left = new TreeNode()
         fillLeftTree(remainingPreorder, inorder.slice(0,inorderIndex), tree.left)
       }
+      console.log('inorder')
       if (inorder.slice(inorderIndex+1).length) {
+        console.log('fillrighttree')
+        console.log(inorder.slice(inorderIndex+1))
+        console.log(remainingPreorder)
         tree.right = new TreeNode()
         fillRightTree(remainingPreorder, inorder.slice(inorderIndex+1), tree.right)
       }
     }
   }
   const fillRightTree = (preorderVals: number[], inorderVals: number[], tree: TreeNode): void => {
+    // console.log('preordervals')
+    // console.log(preorderVals)
     const val: number = preorderVals[0]
     const inorderIndex: number = inorderVals.indexOf(val)
 
@@ -49,12 +54,9 @@ export function buildTree(preorder: number[], inorder: number[]): TreeNode | nul
 
     if (inorder.slice(0, inorderRootIndex).length) {
       result.left = new TreeNode()
-      console.log(remainingPreorder)
-      console.log('fill left')
       fillLeftTree(remainingPreorder, inorder.slice(0,inorderRootIndex), result.left)
     }
     if (inorder.slice(inorderRootIndex+1).length) {
-      console.log('fill right')
       result.right = new TreeNode()
       fillRightTree(remainingPreorder, inorder.slice(inorderRootIndex+1), result.right)
     }
