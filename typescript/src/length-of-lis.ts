66c5b7fbc348c773fdc95e79cc816bc70b91aa5b
sian.izm <sian.izm@gmail.com>
2022-11-12 23:32:29 +0700

    defined lengthOfLIS

--- typescript/src/length-of-lis.ts
+++ typescript/src/length-of-lis.ts
@@ -1,49 +1,14 @@
 export function lengthOfLIS(nums: number[]): number {
-  let array = [nums[0]]
+  let array: number[] = [nums[0]]
 
   for(let i=1; i<=nums.length; i++){
-    if (array[i-1] < nums[i]) {
+    if (array.slice(-1)[0] < nums[i]) {
       array.push(nums[i])
     } else {
-      bisectLeft(array, nums[i])
+      array[bisectLeft(array, nums[i])] = nums[i]
     }
   }
-
-  let lis: number = 0
-  nums.forEach((num, index) => {
-    var count: number = 1
-    console.log('start for loop of j')
-    console.log(num)
-    let currentLargestNum = num
-
-    for(let j=index+1; j<=nums.length; j++) {
-      console.log('j')
-      console.log(j)
-      console.log(nums[j])
-      console.log(currentLargestNum)
-
-      let level = LIS(nums.slice(j), nums[0], 0)
-      if (level > lis) {
-        lis = level
-      }
-      console.log(lis)
-      // if (currentLargestNum < nums[j]) {
-      //   console.log('in if')
-      //   console.log(j)
-      //   console.log(nums[j])
-      //   count++
-      //   currentLargestNum = nums[j]
-      // }
-    }
-    // if (lis < count ) {
-    //   console.log('count')
-    //   console.log(num)
-    //   console.log(count)
-
-    //   lis = count
-    // }
-  })
-  return lis
+  return array.length
 }
 
 function bisectLeft(array: number[], num: number): number {
@@ -51,17 +16,5 @@ function bisectLeft(array: number[], num: number): number {
     if (array[i] > num) {
       return i
     }
-  }
-  return array.length
-}
-
-function LIS(nums: number[], currentLargestNum: number, currentLevel: number): number {
-  nums.shift()
-  if (!nums.length) { return currentLevel }
-
-  if (nums[0] > currentLargestNum) {
-    return LIS(nums, nums[0], currentLevel + 1)
-  } else {
-    return LIS(nums, currentLargestNum, currentLevel)
   }
 }
--- typescript/tests/length-of-lis.test.ts
+++ typescript/tests/length-of-lis.test.ts
@@ -2,19 +2,19 @@ import { expect } from 'chai'
 import { lengthOfLIS } from "../src/length-of-lis"
 
 describe('lenghtOfLIS', () => {
-  // it('should return 4 with nominal case', () => {
-  //   expect(lengthOfLIS([10,9,2,5,3,7,101,18])).to.equal(4)
-  // })
+  it('should return 4 with nominal case', () => {
+    expect(lengthOfLIS([10,9,2,5,3,7,101,18])).to.equal(4)
+  })
 
-  // it('should return 4 with 0', () => {
-  //   expect(lengthOfLIS([0,1,0,3,2,3])).to.equal(4)
-  // })
+  it('should return 4 with 0', () => {
+    expect(lengthOfLIS([0,1,0,3,2,3])).to.equal(4)
+  })
 
   it('should return 4 with 0', () => {
     expect(lengthOfLIS([0,1,3,2,3])).to.equal(4)
   })
 
-  // it('should return 1 with same number array', () => {
-  //   expect(lengthOfLIS([7,7,7,7,7,7])).to.equal(1)
-  // })
+  it('should return 1 with same number array', () => {
+    expect(lengthOfLIS([7,7,7,7,7,7])).to.equal(1)
+  })
 })
