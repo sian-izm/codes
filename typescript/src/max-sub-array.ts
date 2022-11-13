0c5e74abc2e7e66255ec93d9a5f3c1a77b4c82ba
sian.izm <sian.izm@gmail.com>
2022-11-13 22:30:59 +0700

    start implemeting maxSubArray

---
+++ typescript/src/max-sub-array.ts
@@ -0,0 +1,9 @@
+export function maxSubArray(nums: number[]): number[] {
+  let tempMax = nums.slice(-1)
+  let numsReverse = nums.reverse()
+
+  numsReverse.forEach((element, index) => {
+
+  })
+  return numsReverse
+}
