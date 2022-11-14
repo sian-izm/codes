a76e15c9d87ac3fee0f613bdf8dad04c898d01de
sian.izm <sian.izm@gmail.com>
2022-11-14 22:19:01 +0700

    for loop

--- typescript/src/max-sub-array.ts
+++ typescript/src/max-sub-array.ts
@@ -1,9 +1,17 @@
 export function maxSubArray(nums: number[]): number[] {
-  let tempMax = nums.slice(-1)
+  let maxArray = nums.slice(-1)
+  const max = maxArray.reduce((partSum, a) => partSum + a, 0)
+
   let numsReverse = nums.reverse()
 
   numsReverse.forEach((element, index) => {
+    if (element < 0 ){
+      return;
+    }
+    let tempMaxArray: number[] = [element]
+    if (numsReverse[index-1] > 0) {
 
+    }
   })
   return numsReverse
 }
