9ab660ecbd1e36483827e9f8aa3cf6f93f468fb2
sian.izm <sian.izm@gmail.com>
2022-11-19 21:53:08 +0700

    fixed maxSubArray

--- typescript/src/max-sub-array.ts
+++ typescript/src/max-sub-array.ts
@@ -1,20 +1,18 @@
-export function maxSubArray(nums: number[]): number[] {
-  let maxArray = nums.slice(-1)
-
+export function maxSubArray(nums: number[]): number {
   let numsReverse = nums.reverse()
   let max = 0
   let sum = 0
-  numsReverse.forEach((element, index) => {
+  numsReverse.forEach((element) => {
     sum += element
 
     if (sum > max ) {
-      max += sum
+      max = sum
     } else {
       if (sum < 0 ) {
         sum = 0
       }
     }
   })
-  return numsReverse
+  return max
 }
 
--- typescript/tests/max-sub-array.test.ts
+++ typescript/tests/max-sub-array.test.ts
@@ -3,6 +3,6 @@ import { maxSubArray } from '../src/max-sub-array'
 
 describe('maxSubArray',() => {
   it('should return 4 ', () => {
-    expect(maxSubArray([-2,1,-3,4,-1,2,1,-5,4])).to.deep.equal([4,-1,2,1])
+    expect(maxSubArray([-2,1,-3,4,-1,2,1,-5,4])).to.deep.equal(6)
   })
 })
